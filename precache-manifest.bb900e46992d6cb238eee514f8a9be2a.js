ac9211e70bf42e08a745291900acea1209d74332
luca hsieh <chungju.hsieh.ca@gmail.com>
2020-04-14 10:54:17 -0700

    Updates

---
+++ precache-manifest.bb900e46992d6cb238eee514f8a9be2a.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da44a6f37431b788d9c754dd780c88c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1a03f8ae5cd0f835e740",
+    "url": "./static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "1a03f8ae5cd0f835e740",
+    "url": "./static/js/2.80d7236e.chunk.js"
+  },
+  {
+    "revision": "b0ddab79c762e89872ef1603cd4124e8",
+    "url": "./static/js/2.80d7236e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e91180cc3bce772ffa",
+    "url": "./static/js/main.d3114fda.chunk.js"
+  },
+  {
+    "revision": "f0102f0b85917c6ce384",
+    "url": "./static/js/runtime-main.fa3028f1.js"
+  },
+  {
+    "revision": "8fdb680ec8d9ac4a0ce8fef70edc5c56",
+    "url": "./static/media/DictateTube1.8fdb680e.png"
+  },
+  {
+    "revision": "6117fe3d54243db9471abaf8b11df441",
+    "url": "./static/media/DictateTube3.6117fe3d.png"
+  },
+  {
+    "revision": "df45bf0255c7f8322ceece0e781bd646",
+    "url": "./static/media/DictateTube4.df45bf02.png"
+  },
+  {
+    "revision": "86ab97925b37dadeb3646afa7126393d",
+    "url": "./static/media/EasyGrocery1.86ab9792.png"
+  },
+  {
+    "revision": "216c1dcf85c5368d02e604602a984272",
+    "url": "./static/media/EasyGrocery2.216c1dcf.png"
+  },
+  {
+    "revision": "5aaeaa8d46a5967ef3e2be9566bf3972",
+    "url": "./static/media/EasyGrocery3.5aaeaa8d.png"
+  },
+  {
+    "revision": "6673c88c6a182e54a2496cf4dbdccaf4",
+    "url": "./static/media/EasyGrocery4.6673c88c.png"
+  },
+  {
+    "revision": "90571f0920b922e2d520e0d94ad8e787",
+    "url": "./static/media/EasyGrocery5.90571f09.png"
+  },
+  {
+    "revision": "4f452a516242d7692df81d449fb251ee",
+    "url": "./static/media/Fero.4f452a51.png"
+  },
+  {
+    "revision": "f61cf9ac847a3f0f37f159b9e571428c",
+    "url": "./static/media/MineSweeper.f61cf9ac.png"
+  },
+  {
+    "revision": "e0bc08aac8fd57f8ee79cfe83ad69bef",
+    "url": "./static/media/ProjectMgmt.e0bc08aa.png"
+  },
+  {
+    "revision": "ae7c0bb3fa33666fd19e2e7d28dfac82",
+    "url": "./static/media/ProjectMgmt1.ae7c0bb3.png"
+  },
+  {
+    "revision": "92e6589356ae30124f46769192aecee5",
+    "url": "./static/media/ProjectMgmt10.92e65893.png"
+  },
+  {
+    "revision": "5139c448a44b2df97e056bb172212e44",
+    "url": "./static/media/ProjectMgmt2.5139c448.png"
+  },
+  {
+    "revision": "1adbffffc8b8ed377c25bacc9ca6b75b",
+    "url": "./static/media/ProjectMgmt3.1adbffff.png"
+  },
+  {
+    "revision": "d767564ef462b0e5849506abac9d88a0",
+    "url": "./static/media/ProjectMgmt4.d767564e.png"
+  },
+  {
+    "revision": "d788a9faa4ae9023707abe1c83f7b5de",
+    "url": "./static/media/ProjectMgmt5.d788a9fa.png"
+  },
+  {
+    "revision": "db6706ae3f6a0899ae06ba167579a3b1",
+    "url": "./static/media/ProjectMgmt6.db6706ae.png"
+  },
+  {
+    "revision": "f0836755d3d2020632ed261e4885733e",
+    "url": "./static/media/ProjectMgmt7.f0836755.png"
+  },
+  {
+    "revision": "4a4437fa0d70e11179f02f94f58033f8",
+    "url": "./static/media/ProjectMgmt8.4a4437fa.png"
+  },
+  {
+    "revision": "e66eaddef49b5fffc7a7a618c741a2ce",
+    "url": "./static/media/ProjectMgmt9.e66eadde.png"
+  },
+  {
+    "revision": "0bfee9fd8ec7dc9f04b1aff434a3f82d",
+    "url": "./static/media/TravelPacker1.0bfee9fd.png"
+  },
+  {
+    "revision": "c7e2dd2f9ded618539c190451a644d86",
+    "url": "./static/media/TravelPacker2.c7e2dd2f.png"
+  },
+  {
+    "revision": "66cfe524244db96978445ac9d93f411e",
+    "url": "./static/media/TravelPacker3.66cfe524.png"
+  },
+  {
+    "revision": "6e08f5bb654c50bdfd31303c0d0bb0a1",
+    "url": "./static/media/VanArt1.6e08f5bb.png"
+  },
+  {
+    "revision": "a0a74917cf5cceb773313baf7c9ef68b",
+    "url": "./static/media/VanArt2.a0a74917.png"
+  },
+  {
+    "revision": "fbe934bae93f3c52057bb9637854a89e",
+    "url": "./static/media/experience_workshop1.fbe934ba.png"
+  },
+  {
+    "revision": "a551b19edea3d5588ceeeac2f1689afa",
+    "url": "./static/media/logo_lg_white.a551b19e.png"
+  },
+  {
+    "revision": "34dbefad792fab0aaaa7c0d6762b4693",
+    "url": "./static/media/mypicture_sm.34dbefad.png"
+  },
+  {
+    "revision": "5a9fd6f6498bdcf212dcd3af7e7e7e59",
+    "url": "./static/media/project_MineSweeper_icon-01.5a9fd6f6.png"
+  },
+  {
+    "revision": "ed74ed5b0d830792acad09a7bfd96997",
+    "url": "./static/media/project_bcitHackthon-01.ed74ed5b.png"
+  },
+  {
+    "revision": "dc7f3969d87d79ce0a6c80c86f3d1399",
+    "url": "./static/media/project_dictateTube.dc7f3969.png"
+  },
+  {
+    "revision": "5199a95cc4ef38e2b8716d4610429219",
+    "url": "./static/media/project_easyGrocery.5199a95c.png"
+  },
+  {
+    "revision": "8d147d41924cc484980f3d65cae06146",
+    "url": "./static/media/project_makeplus.8d147d41.png"
+  },
+  {
+    "revision": "119e23ef9409223a003f6ebb4a2bdd2e",
+    "url": "./static/media/project_publicArt.119e23ef.png"
+  },
+  {
+    "revision": "ba767426b0b0444832144e9475c57605",
+    "url": "./static/media/project_travelPacker.ba767426.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactTest/precache-manifest.3a3e8cb7b580098bef3dff7309e340e5.js"
+  "./precache-manifest.bb900e46992d6cb238eee514f8a9be2a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactTest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
